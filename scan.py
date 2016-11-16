a65a681b36d8e3cc49f709e9db1743a244facdf3
Fabrizio Colonna <colofabrix@tin.it>
2016-11-16 01:33:42 +0100

    Progress...

--- scan.py
+++ scan.py
@@ -8,19 +8,29 @@ import numpy as np
 import argparse
 import cv2
 
+# Parameters
+CLAHE_CLIP_LIMIT = 1.5
+CLAHE_GRID_SIZE = 8
+BLUR_KERNEL = 7
+CANNY_MINVAL = 100
+CANNY_MAXVAL = 150
+CANNY_SOBEL_SIZE = 3
+DILATE_KERNEL = 20
+DILATE_ITERATIONS = 1
+APPROX_PRECISION = 0.02
+COVERING_AREA = 0.25
+
 def pre_process(img):
 	img   = img.copy()
-	clahe = cv2.createCLAHE(clipLimit = 2.0, tileGridSize = (8, 8))
+	clahe = cv2.createCLAHE(clipLimit = CLAHE_CLIP_LIMIT, tileGridSize = (CLAHE_GRID_SIZE, CLAHE_GRID_SIZE))
 
 	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	cv2.imshow("Grayscale", img)
+	img = clahe.apply(img)
+	cv2.imshow("Equalized", img)
 
-	#img = clahe.apply(img)
-	#cv2.imshow("Equalized", img)
-
-	img = cv2.GaussianBlur(img, (3, 3), 0)
-	img = cv2.Canny(img, 100, 250, 5)
-	img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, (3, 3))
+	img = cv2.medianBlur(img, BLUR_KERNEL)
+	img = cv2.Canny(img, CANNY_MINVAL, CANNY_MAXVAL, CANNY_SOBEL_SIZE)
+	img = cv2.dilate(img, (DILATE_KERNEL, DILATE_KERNEL), iterations = DILATE_ITERATIONS)
 	cv2.imshow("Closed", img)
 
 	cv2.waitKey(0)
@@ -35,50 +45,52 @@ args = vars(ap.parse_args())
 
 # Loading
 image = cv2.imread(args["image"])
-ratio = image.shape[0] / 500.0
 image = imutils.resize(image, height = 500)
-orig  = image.copy()
 
+width, height = image.shape[:2]
+image_area = width * height
+
+orig  = image.copy()
 image = pre_process(image)
 
+def contour_metric(contour):
+	return cv2.contourArea(cv2.convexHull(contour))
+
 # find the contours in the edged image, keeping only the largest ones, and initialize the screen contour
-im2, cnts, hierarchy = cv2.findContours(image.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE )
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)
+im2, contours, hierarchy = cv2.findContours(image.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE )
+contours = sorted(contours, key = contour_metric, reverse = True)
 
 # loop over the contours
-for c in cnts:
+big_shapes = []
+for c in contours:
 	# approximate the contour
-	approx = cv2.approxPolyDP(c, 0.02 * cv2.arcLength(c, True), True)
+	approx = cv2.approxPolyDP(c, APPROX_PRECISION * cv2.arcLength(c, True), True)
+	hull = cv2.convexHull(approx)
+	hull_area = cv2.contourArea(hull)
 
-	# if our approximated contour has four points, then we
-	# can assume that we have found our screen
-	if len(approx) < 4 or cv2.contourArea(approx) < 500: continue
+	if len(hull) > 4 and hull_area >= image_area * COVERING_AREA:
+		big_shapes.append(hull)
 
-	print("Area: %s" % cv2.contourArea(approx))
-	print("Points: %s" % len(approx))
-	print
 
-	# show the contour (outline) of the piece of paper
-	with_contours = orig.copy()
-	cv2.drawContours(with_contours, [approx], -1, (0, 0, 255), 1)
-	cv2.drawContours(with_contours, approx, -1, (255, 0, 0), 3)
-	cv2.imshow("With contours", with_contours)
+# Order by number of points, prefer less points
+contours = sorted(big_shapes, key = len)
 
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+if len(contours) == 0:
+	print "No paper-shape found. Exit."
+	exit()
 
-	with_contours = orig.copy()
-	ch = cv2.convexHull(approx)
-	cv2.drawContours(with_contours, [ch], -1, (0, 0, 255), 1)
-	cv2.drawContours(with_contours, ch, -1, (255, 0, 0), 3)
-	cv2.imshow("Convext Hull", with_contours)
+paper = contours[0]
+hull = cv2.convexHull(paper)
 
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
-
-	if len(approx) == 4:
-		screenCnt = approx
-		break
+# show the contour (outline) of the piece of paper
+with_contours = orig.copy()
+cv2.drawContours(with_contours, [paper], -1, (0, 0, 255), 2)
+cv2.drawContours(with_contours, paper, -1, (255, 0, 0), 4)
+cv2.drawContours(with_contours, [hull], -1, (0, 255, 0), 2)
+cv2.drawContours(with_contours, hull, -1, (0, 255, 0), 4)
+cv2.imshow("With contours", with_contours)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 exit()
 
