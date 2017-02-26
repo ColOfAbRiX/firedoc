bd8a38e430f4189cc7ba40fcadda59d9c0f716f8
= <freddie.poser@gmail.com>
2017-02-26 18:56:56 +0000

    Work on hough transform

--- scan_hough.py
+++ scan_hough.py
@@ -45,10 +45,18 @@ def pre_process(image):
     
     return image
 
+def get_line_mq(rho, theta):
+    m = - math.cos(theta) / math.sin(theta)
+    q = rho / math.sin(theta)
+    return m, q
+
+
 def rt_to_xy(rho, theta):
-    a = np.cos(theta); b = np.sin(theta)
+    a = np.cos(theta)
+    b = np.sin(theta)
     
-    x0 = a * rho; y0 = b * rho
+    x0 = a * rho
+    y0 = b * rho
 
     x1 = int(x0 + 1000 * (-b))
     y1 = int(y0 + 1000 * (a))
@@ -88,9 +96,18 @@ orig_image  = image.copy()
 image = pre_process(image)
 show("Image", image)
 
+print(image)
+
+thresh_image = image.copy()
+ret, thresh_image = cv2.threshold(thresh_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+display_thresh = cv2.cvtColor(thresh_image, cv2.COLOR_GRAY2RGB)
+show("Threshold", thresh_image)
+
 # Edges
 image = cv2.Canny(image, CANNY_MINVAL, CANNY_MAXVAL, apertureSize=3)
 show("Edges", image)
+display_edges = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+
 
 #
 # Lines detection
@@ -119,7 +136,22 @@ lines = af_v.cluster_centers_.tolist() + af_h.cluster_centers_.tolist()
 
 display_image = orig_image.copy()
 for rho, theta in lines:
-    print("Rho: %f, Theta: %f" % (rho, theta))
     x1, y1, x2, y2 = rt_to_xy(rho, theta)
     display_image = cv2.line(display_image, (x1,y1), (x2,y2), (0, 255, 255), 2)
+    display_thresh = cv2.line(display_thresh, (x1,y1), (x2,y2), (255, 0, 0), 1) 
+    display_edges = cv2.line(display_edges, (x1,y1), (x2,y2), (255, 0, 0), 1)
 show("Image", display_image)
+show("Image", display_thresh)
+show("Image", display_edges)
+
+for rho,theta in lines:
+    #TODO: handle the case
+    if (theta == 0):
+        continue
+
+    m, q = get_line_mq(rho,theta)
+    for x in range(0, int(ratio * WORKING_HEIGHT)):
+        y = round(m * x + q)
+        print((x,y))
+
+
